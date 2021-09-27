f8ae9833ebe393e76c26b7a60c7c249585cc0eb4
shreya soni <shreyasoni@shreyas-MacBook-Pro.local>
2021-09-26 22:10:31 -0400

    first commit

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.Date;
+
+/**
+ *
+ * @author shreyasoni
+ */
+public class Person {
+    
+    private String name;
+    private String geographic;
+    private String dob;
+    private String telephone;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String medical;
+    private String health;
+    private String baccount;
+    private String license;
+    private String vechicle;
+    private String serial;
+    private String linkedin;
+    private String ip;
+    private String biometric;
+    private String photo;
+    private String unique;
+    private String deviceIdentifier;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographic() {
+        return geographic;
+    }
+
+    public void setGeographic(String geographic) {
+        this.geographic = geographic;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedical() {
+        return medical;
+    }
+
+    public void setMedical(String medical) {
+        this.medical = medical;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getBaccount() {
+        return baccount;
+    }
+
+    public void setBaccount(String baccount) {
+        this.baccount = baccount;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getVechicle() {
+        return vechicle;
+    }
+
+    public void setVechicle(String vechicle) {
+        this.vechicle = vechicle;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getUnique() {
+        return unique;
+    }
+
+    public void setUnique(String unique) {
+        this.unique = unique;
+    }
+    
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+    
+}
